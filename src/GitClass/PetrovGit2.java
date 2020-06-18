ef611389420f377eda03323554b0afc7c1bbf5d2
Petrov452 <petrov.andrey.chop@gmail.com>
2020-06-18 11:18:21 -0400

    Second code push

--- src/GitClass/PetrovGit2.java
+++ src/GitClass/PetrovGit2.java
@@ -1,11 +1,10 @@
 package GitClass;
 
-public class GitClassTest2 {
+public class PetrovGit2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    System.out.println("SyntaxTechnologies");
-
+System.out.println("HOw arre you doing today?");
 	}
 
 }
---
+++ src/GitClass/PetrovGit3.java
@@ -0,0 +1,10 @@
+package GitClass;
+
+public class PetrovGit3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("HOw arre you doing today?");
+	}
+
+}
